ef051a6f6f0ec579d13700ae3755cddb6218f1cf
Rodrigo0428 <rodrigocarrasco2004@gmail.com>
2024-05-23 22:51:04 -0500

    hola

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppBanco.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet <AppBanco.Models.CuentaBancaria> DataCuentaBancaria {get; set; }
+    public DbSet <AppBanco.Models.Cuenta> Datacuenta {get; set; }
+}
